6bda5a947ef7622532390ae4c9b4366651f17238
Marcos da Silva Jr <marcos_junior1001@hotmail.com>
2025-10-07 22:49:51 -0500

    On a demo I realized I had a small issue on showing a error message. So, small fix on that.

--- scheduler-client/src/utils/madalMessageUtils.js
+++ scheduler-client/src/utils/madalMessageUtils.js
@@ -4,10 +4,10 @@
    * @returns {string} - The formatted error message.
    */
   export const formatError = (err) => {
-
+    console.log('formatError', err[0].data.message.errors)
     if (!err) return 'Unknown error'
     if (typeof err === 'string') return err
-    if (Array.isArray(err)) return err[0].data.errors[0].message
+    if (Array.isArray(err)) return ( err[0].data.message || JSON.stringify(err))
     if (err.data) return typeof err.data === 'string' ? err.data : err.data.message + (err.data.errors ? `: ${JSON.stringify(err.data.errors)}` : '')
     if (err.message) return err.message
     try { return JSON.stringify(err) } catch (e) { return String(err) }
